7fe91b2cca31b92d5f27574525a9c1a9fe66c66c
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-04 10:44:32 -0400

    capturando datos del formulario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
 import { RegisterScreenComponent } from './pages/register-screen/register-screen.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegisterScreenComponent } from './pages/register-screen/register-screen
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/login-screen/login-screen.component.ts
+++ src/app/auth/pages/login-screen/login-screen.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
-  styleUrls: ['./login-screen.component.scss']
+  styleUrls: ['./login-screen.component.scss'],
 })
 export class LoginScreenComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    email: ['test1@mail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
