17cd2b1c315291b7e32c35f895211c49ac8d32f3
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-04 14:41:21 -0400

    formulario reactivo para el registro y obteniendo los datos

--- src/app/auth/pages/register-screen/register-screen.component.ts
+++ src/app/auth/pages/register-screen/register-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register-screen',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterScreenComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    username: ['text 8', [Validators.required, Validators.minLength(3)]],
+    email: ['test1@mail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+    password2: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  register() {
+    console.log(this.miFormulario.value);
+    // this.authService.login(this.miFormulario.value).subscribe((res) => {
+    //   if (res === true) {
+    //     // almacenar usuario en el localStogare y stringify es para parsear el json
+    //     localStorage.setItem('user', JSON.stringify(this.authService.user))
+    //     this.router.navigateByUrl("/task")
+    //   }else{
+    //     Swal.fire({
+    //       icon: 'error',
+    //       title: 'Error ',
+    //       text: res,
+    //     })
+    //   }
+    // });
+
   }
-
 }
