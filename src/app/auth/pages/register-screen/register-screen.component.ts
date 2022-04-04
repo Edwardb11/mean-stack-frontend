eafc6bf5b580453991ce9dd8de9d060601bad39f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-04-04 14:55:16 -0400

    Registro (#3)
    * formulario reactivo para el registro y obteniendo los datos
    
    * logica de registro implementada junto a la de confirmar la contraseña

--- src/app/auth/pages/register-screen/register-screen.component.ts
+++ src/app/auth/pages/register-screen/register-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register-screen',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterScreenComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    username: ['text 8', [Validators.required, Validators.minLength(3)]],
+    email: ['test1@mail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+    password2: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  register() {
+const {password,password2}=this.miFormulario.value
+
+if (password === password2) {
+  
+  this.authService.register(this.miFormulario.value).subscribe((res) => {
+    if (res === true) {
+      // almacenar usuario en el localStogare y stringify es para parsear el json
+      localStorage.setItem('user', JSON.stringify(this.authService.user))
+      this.router.navigateByUrl("/task")
+      }else{
+        Swal.fire({
+          title: 'Error ',
+          text: res,
+          icon: 'error',
+        })
+      }
+    });
+  }else{
+    Swal.fire({
+      title: 'Error ',
+      text: 'Las contraseñas no coinciden',
+      icon: 'error',
+    })
   }
-
+    
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, observable, of, Subscriber, tap } from 'rxjs';
+import {
+  catchError,
+  map,
+  Observable,
+  observable,
+  of,
+  Subscriber,
+  tap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -34,18 +42,37 @@ export class AuthService {
         catchError((err) => of(err.error.msg))
       );
   }
-  validarToken():Observable<boolean>{
-    const token = JSON.parse(localStorage.getItem('user')!)
+
+  register(data: { username:string, email: string; password: string }) {
+    return this.httpCliennt
+      .post<any>('http://localhost:3000/auth/register', data)
+      .pipe(
+        // Procesar abtres de la respuesta,filtrar solo lo que nos interesa
+        tap((res) => {
+          if (res.ok === true) {
+            this._user = {
+              id: res.id,
+              username: res.username,
+              token: res.token,
+            };
+          } else {
+            this._user = null;
+          }
+        }),
+        map((res) => res.ok),
+        catchError((err) => of(err.error.msg))
+      );
+  }
+  validarToken(): Observable<boolean> {
+    const token = JSON.parse(localStorage.getItem('user')!);
     if (token) {
-      return new Observable((Subscriber)=>{
+      return new Observable((Subscriber) => {
         Subscriber.next(true);
-      }) 
-    }
-    else{
-      return new Observable((Subscriber)=>{
+      });
+    } else {
+      return new Observable((Subscriber) => {
         Subscriber.next(false);
-      }) 
+      });
     }
-    
   }
 }
