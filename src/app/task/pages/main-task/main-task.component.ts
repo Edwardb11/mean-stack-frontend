e3157707c715bb81c436a1a8c4f77ef64a4ceb68
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-04-04 12:50:12 -0400

    leyendo la informacion desde el localStogare (#1)

--- src/app/task/pages/main-task/main-task.component.ts
+++ src/app/task/pages/main-task/main-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
 
 @Component({
   selector: 'app-main-task',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainTaskComponent implements OnInit {
 
-  constructor() { }
+  user:any
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    // llamar al user desde el servicio
+    // this.user=this.authService.user
+    // llamar ak user desde localStogare
+    this.user=JSON.parse(localStorage.getItem('user')||'');
   }
 
 }
