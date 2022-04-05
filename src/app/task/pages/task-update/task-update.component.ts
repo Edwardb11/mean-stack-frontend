9393cb31495780844f6819de74ea32760fd77020
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-05 16:10:01 -0400

    captuando datos desde el lado del formulario

--- src/app/task/pages/task-update/task-update.component.ts
+++ src/app/task/pages/task-update/task-update.component.ts
@@ -6,28 +6,27 @@ import { CrudService } from 'src/app/services/crud.service';
 @Component({
   selector: 'app-task-update',
   templateUrl: './task-update.component.html',
-  styleUrls: ['./task-update.component.scss']
+  styleUrls: ['./task-update.component.scss'],
 })
 export class TaskUpdateComponent implements OnInit {
-  task: any = {};
   user: any;
   miFormulario: FormGroup = this.formBuilder.group({
-    newTask: [' ', Validators.required],
+    newTask: [''],
   });
   constructor(
     private crudService: CrudService,
     private router: Router,
-    private activatedRoute:ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.user = this.crudService.user;
-    this.activatedRoute.params.subscribe(params => {console.log(params);})
-
+    this.activatedRoute.params.subscribe((params) => {
+      this.miFormulario.setValue({ newTask: params['nombre'] });
+    });
   }
-  update(){
-
+  update() {
+    console.log(this.miFormulario.value.newTask);
   }
-
 }
